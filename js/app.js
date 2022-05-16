984541953397d32aacaecfcec9c8f01265621742
김성민 <mary5544@gimseongmin-ui-MacBookPro.local>
2022-05-16 12:29:13 +0900

    ver 0.1.5

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.nav-inner li');
+let logoLinks = document.querySelectorAll('.logo');
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e){
+  mouseCursor.style.top = e.pageY + 'px';
+  mouseCursor.style.left = e.pageX + 'px';
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove("link-grow");
+  });
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add("link-grow");
+  });
+});
+
+logoLinks.forEach(link => {
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove("link-grow");
+  });
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add("link-grow");
+  });
+});
