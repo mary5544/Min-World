b93761fc38392f02771243c07a66f4e183ae60e2
sungmin <mary5544@naver.com>
2023-11-24 18:07:33 +0900

    리뉴얼 안내

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
-let mouseCursor = document.querySelector('.cursor');
-let navLinks = document.querySelectorAll('.nav-inner li');
-let logoLinks = document.querySelectorAll('.logo');
-
-window.addEventListener('mousemove', cursor);
-
-function cursor(e){
-  mouseCursor.style.top = e.pageY + 'px';
-  mouseCursor.style.left = e.pageX + 'px';
-}
-
-navLinks.forEach(link => {
-  link.addEventListener('mouseleave', () => {
-    mouseCursor.classList.remove("link-grow");
+(function(){
+  let mouseCursor = document.querySelector('.cursor');
+  let navLinks = document.querySelectorAll('.nav-inner li');
+  let logoLinks = document.querySelectorAll('.logo');
+  
+  window.addEventListener('mousemove', cursor);
+  
+  function cursor(e){
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left = e.pageX + 'px';
+  }
+  
+  navLinks.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+      mouseCursor.classList.remove("link-grow");
+    });
+    link.addEventListener('mouseover', () => {
+      mouseCursor.classList.add("link-grow");
+    });
   });
-  link.addEventListener('mouseover', () => {
-    mouseCursor.classList.add("link-grow");
+  
+  logoLinks.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+      mouseCursor.classList.remove("link-grow");
+    });
+    link.addEventListener('mouseover', () => {
+      mouseCursor.classList.add("link-grow");
+    });
   });
-});
+})();
 
-logoLinks.forEach(link => {
-  link.addEventListener('mouseleave', () => {
-    mouseCursor.classList.remove("link-grow");
+(function(){
+  $('#popupBanner .xBtn').click(function(){
+    $('#popupBanner').hide();
   });
-  link.addEventListener('mouseover', () => {
-    mouseCursor.classList.add("link-grow");
-  });
-});
+})();
